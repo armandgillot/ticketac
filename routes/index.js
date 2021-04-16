1e669a3af6661e8acad9138b6316cd459df76371
Armand <armand.gillot@gmail.com>
2021-04-16 14:07:03 +0200

    route users.js

--- routes/index.js
+++ routes/index.js
@@ -108,48 +108,4 @@ router.get('/mylasttrips', async (req, res, next) => {
     res.render('myLastTrips', {liste});
   });
 
-// route connexion - utilisateur a déjà un compte dans l'app 
-router.post('/sign-in', async function (req, res, next) {
-  var searchUser = await usersModel.findOne({
-    email: req.body.emailFromFront,
-    password: req.body.passwordFromFront
-  })
-
-  if (searchUser != null) {
-    req.session.user = {
-      name: searchUser.username,
-      id: searchUser._id
-    }
-    console.log(req.session.user.id);
-    res.render('index', { searchResult })
-  } else {  //sinon 
-    res.redirect('/')
-  }
-});
-
-router.post('/sign-up', async (req, res, next) => {
-
-  if (await usersModel.findOne({ email: req.body.emailFromFront })) {
-    res.redirect('/');
-  } else {
-    var newUsers = new usersModel({
-      username: req.body.usernameFromFront,
-      userfirstname: req.body.userfirstnameFromFront,
-      email: req.body.emailFromFront,
-      password: req.body.passwordFromFront,
-    });
-    var usersSaved = await newUsers.save();
-
-    req.session.user = { name: newUsers.username, id: newUsers._id };
-
-    res.render('index', {searchResult});
-  }
-});
-
-// Logout
-router.get('/logout', async (req, res, next) => {
-  req.session.user = null;
-  res.redirect('/');
-});
-
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const mongoose = require('mongoose');
+var usersModel = require('../models/users');
+var searchResult = false;
+
+// route connexion - utilisateur a déjà un compte dans l'app 
+router.post('/sign-in', async function (req, res, next) {
+  var searchUser = await usersModel.findOne({
+    email: req.body.emailFromFront,
+    password: req.body.passwordFromFront
+  })
+
+  if (searchUser != null) {
+    req.session.user = {
+      name: searchUser.username,
+      id: searchUser._id
+    }
+    console.log(req.session.user.id);
+    res.render('index', { searchResult })
+  } else {  //sinon 
+    res.redirect('/')
+  }
+});
+
+router.post('/sign-up', async (req, res, next) => {
+
+  if (await usersModel.findOne({ email: req.body.emailFromFront })) {
+    res.redirect('/');
+  } else {
+    var newUsers = new usersModel({
+      username: req.body.usernameFromFront,
+      userfirstname: req.body.userfirstnameFromFront,
+      email: req.body.emailFromFront,
+      password: req.body.passwordFromFront,
+    });
+    var usersSaved = await newUsers.save();
+
+    req.session.user = { name: newUsers.username, id: newUsers._id };
+
+    res.render('index', {searchResult});
+  }
+});
+
+// Logout
+router.get('/logout', async (req, res, next) => {
+  req.session.user = null;
+  res.redirect('/');
 });
 
 module.exports = router;
